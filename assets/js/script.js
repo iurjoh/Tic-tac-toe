c6571c20fce0c51be56a51eb983a8a668bfc342a
Iuri Johansson <iuri_abreu@hotmail.com>
2022-09-26 17:41:54 +0000

    Add all rules in js to start, win, draw, restart, switch icons between turns and message to win and draw at end game

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,123 @@
+// selecting all data from cells
 const cellElements = document.querySelectorAll("[data-cell]");
 
+// selecting data from the board
+const board = document.querySelector("[data-board]");
+
+// All text for end game messages
+const winningMessageTextElement = document.querySelector(
+  "[data-winning-message-text]"
+);
+const winningMessage = document.querySelector("[data-winning-message]");
+
+// restart button
+const restartButton = document.querySelector("[data-restart-button]");
+
+// disable and insert in "startGame const"
 let isCircleTurn;
 
-const handleClick = (i) => {
-    // set X or O
-    const cell = i.target;
-    const classToAdd = isCircleTurn(i) ? "o" : "x".
-    // check if won
+// winning combinations
+const winningCombinations = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
 
-    // check if drew
+// each cell clicked will be filled only once
+const startGame = () => {
+  isCircleTurn = false;
 
-    // switch between icons X and O
+  for (const cell of cellElements) {
+    cell.classList.remove("circle");
+    cell.classList.remove("x");
+    cell.removeEventListener("click", handleClick);
+    cell.addEventListener("click", handleClick, { once: true });
+  }
+
+  setBoardHoverClass();
+  winningMessage.classList.remove("show-winning-message");
 };
 
-for (const cell of cellElements) {
-    cellElements.addEventListener("click", handleClick, {once: true};
-}
+// end game and restart button
+const endGame = (isDraw) => {
+  if (isDraw) {
+    winningMessageTextElement.innerText = "Draw!";
+  } else {
+    winningMessageTextElement.innerText = isCircleTurn
+      ? "Circle Won!"
+      : "X Won!";
+  }
 
+  winningMessage.classList.add("show-winning-message");
+};
+
+// check for win
+const checkForWin = (currentPlayer) => {
+  return winningCombinations.some((combination) => {
+    return combination.every((index) => {
+      return cellElements[index].classList.contains(currentPlayer);
+    });
+  });
+};
+
+// check for draw
+const checkForDraw = () => {
+  return [...cellElements].every((cell) => {
+    return cell.classList.contains("x") || cell.classList.contains("circle");
+  });
+};
+
+const placeMark = (cell, classToAdd) => {
+  cell.classList.add(classToAdd);
+};
+
+// hover
+const setBoardHoverClass = () => {
+  board.classList.remove("circle");
+  board.classList.remove("x");
+
+  if (isCircleTurn) {
+    board.classList.add("circle");
+  } else {
+    board.classList.add("x");
+  }
+};
+
+// switch icons X and O between turns
+const swapTurns = () => {
+  isCircleTurn = !isCircleTurn;
+
+  setBoardHoverClass();
+};
+
+const handleClick = (e) => {
+  // set X or O
+  const cell = e.target;
+  const classToAdd = isCircleTurn ? "circle" : "x";
+
+  placeMark(cell, classToAdd);
+
+  // check if win
+  const isWin = checkForWin(classToAdd);
+
+  // check if draw
+  const isDraw = checkForDraw();
+
+  if (isWin) {
+    endGame(false);
+  } else if (isDraw) {
+    endGame(true);
+  } else {
+    // switch between icons X and O
+    swapTurns();
+  }
+};
+
+startGame();
+
+restartButton.addEventListener("click", startGame);
